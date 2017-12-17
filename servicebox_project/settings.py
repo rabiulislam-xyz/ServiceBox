df88716df1a6c70b6ec916ade33e2dc0f9320578
rabiulislam993 <rabiulislam993@live.com>
2017-12-17 14:51:20 +0600

    heroku configuration added

--- servicebox_project/settings.py
+++ servicebox_project/settings.py
@@ -124,11 +124,19 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # login/logout/password change settings
@@ -136,7 +144,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/registration/login/'
 
 
-# Heroku configuration
+# Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
