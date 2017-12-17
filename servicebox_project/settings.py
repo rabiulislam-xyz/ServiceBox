c1beffc0a3610e5f3ed67cda84e9df87f043b30d
rabiulislam993 <rabiulislam993@live.com>
2017-12-17 14:38:36 +0600

    heroku configuration added

--- servicebox_project/settings.py
+++ servicebox_project/settings.py
@@ -141,7 +141,3 @@ import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
