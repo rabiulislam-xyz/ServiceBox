9b237a979c96436e5aa27fb8a30755df34b0047f
rabiulislam993 <rabiulislam993@live.com>
2017-12-18 11:33:33 +0600

    added search functionality, builtin and algolia

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import home
+from .views import home, search
 
 
 app_name = 'website'
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^search/$', search, name='search'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,15 +1,34 @@
+from django.db.models import Q
 from django.shortcuts import render
 from algoliasearch_django import raw_search
 
 from service.models import Service
+from .forms import SearchForm
 
 def home(request):
     context = {}
     top_service_list = Service.objects.all()
 
-    if request.method == 'POST':
-        search_result = raw_search(Service, request.POST.get('q'))
-        context["search_result"] = search_result
-
     context['top_service_list'] = top_service_list
     return render(request, 'website/home.html', context)
+
+
+def search(request):
+    context = {}
+    print(request.GET)
+    q = request.GET.get('q')
+    if q:
+        # setting custom search result
+        results = Service.objects.filter(Q(name__icontains=q)|
+                                         Q(description__icontains=q)|
+                                         Q(provider__first_name__contains=q)|
+                                         Q(provider__last_name__contains=q)).distinct()
+        context['results']= results
+
+        # adding algolia search result
+        algolia_results = raw_search(Service, q)
+        context["algolia_results"] = algolia_results
+
+    form = SearchForm(request.GET or None)
+    context['form']=form
+    return render(request, 'website/search_result.html', context)
